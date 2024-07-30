79acdab349ce9b5d52c4026be14483372c03c930
kantapon.h <kantapon.h@bossup.co.th>
2024-07-31 00:52:31 +0700

    init project

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,234 @@
+--  ╭─────────────────────────────────────────────────────────────────────────────╮
+--  │            Keymaps are automatically loaded on the VeryLazy event           │
+--  │                     Default keymaps that are always set:                    │
+--  │ https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua │
+--  ╰─────────────────────────────────────────────────────────────────────────────╯
+
+local util = require("util")
+local mapkey = require("util.keymapper").mapvimkey
+
+-- Buffer Navigation
+mapkey("<leader>bn", "bnext", "n") -- Next buffer
+mapkey("<leader>bp", "bprevious", "n") -- Prev buffer
+mapkey("<leader>bb", "e #", "n") -- Switch to Other Buffer
+mapkey("<leader>`", "e #", "n") -- Switch to Other Buffer
+
+-- Directory Navigatio}n
+mapkey("<leader>m", "NvimTreeFocus", "n")
+mapkey("<leader>e", "NvimTreeToggle", "n")
+
+-- Pane and Window Navigation
+mapkey("<C-h>", "<C-w>h", "n") -- Navigate Left
+mapkey("<C-j>", "<C-w>j", "n") -- Navigate Down
+mapkey("<C-k>", "<C-w>k", "n") -- Navigate Up
+mapkey("<C-l>", "<C-w>l", "n") -- Navigate Right
+mapkey("<C-h>", "wincmd h", "t") -- Navigate Left
+mapkey("<C-j>", "wincmd j", "t") -- Navigate Down
+mapkey("<C-k>", "wincmd k", "t") -- Navigate Up
+mapkey("<C-l>", "wincmd l", "t") -- Navigate Right
+mapkey("<C-h>", "TmuxNavigateLeft", "n") -- Navigate Left
+mapkey("<C-j>", "TmuxNavigateDown", "n") -- Navigate Down
+mapkey("<C-k>", "TmuxNavigateUp", "n") -- Navigate Up
+mapkey("<C-l>", "TmuxNavigateRight", "n") -- Navigate Right
+
+-- Window Management
+mapkey("<leader>sv", "vsplit", "n") -- Split Vertically
+mapkey("<leader>sh", "split", "n") -- Split Horizontally
+mapkey("<C-Up>", "resize +2", "n")
+mapkey("<C-Down>", "resize -2", "n")
+mapkey("<C-Left>", "vertical resize +2", "n")
+mapkey("<C-Right>", "vertical resize -2", "n")
+
+-- Show Full File-Path
+mapkey("<leader>pa", "ShowPath", "n") -- Show Full File Path
+
+-- Indenting
+vim.keymap.set("v", "<", "<gv", { silent = true, noremap = true })
+vim.keymap.set("v", ">", ">gv", { silent = true, noremap = true })
+
+-- change word with <c-c>
+vim.keymap.set("n", "<C-c>", "<cmd>normal! ciw<cr>a")
+
+-- htop
+if vim.fn.executable("htop") == 1 then
+  vim.keymap.set("n", "<leader>xh", function()
+    require("lazyvim.util").float_term({ "htop" })
+  end, { desc = "htop" })
+end
+
+local function do_open(uri)
+  local cmd, err = vim.ui.open(uri)
+  local rv = cmd and cmd:wait(1000) or nil
+  if cmd and rv and rv.code ~= 0 then
+    err = ("vim.ui.open: command %s (%d): %s"):format(
+      (rv.code == 124 and "timeout" or "failed"),
+      rv.code,
+      vim.inspect(cmd.cmd)
+    )
+  end
+  return err
+end
+
+--  ╭───────────────────────────────────────────────────────────╮
+--  │ Credit: June Gunn <Leader>?/! | Google it / Feeling lucky │
+--  ╰───────────────────────────────────────────────────────────╯
+---@param pat string
+local function google(pat)
+  local query = '"' .. vim.fn.substitute(pat, '["\n]', " ", "g") .. '"'
+  query = vim.fn.substitute(query, "[[:punct:] ]", [[\=printf("%%%02X", char2nr(submatch(0)))]], "g")
+  do_open("https://www.google.com/search?" .. "q=" .. query)
+end
+
+vim.keymap.set("n", "<leader>?", function()
+  google(vim.fn.expand("<cWORD>"))
+end, { desc = "Google" })
+
+vim.keymap.set("x", "<leader>?", function()
+  google(vim.fn.getreg("g"))
+end, { desc = "Google" })
+
+--  ╭────────────────────────────────────╮
+--  │ GX - replicate netrw functionality │
+--  ╰────────────────────────────────────╯
+local function open_link()
+  local url = vim.ui._get_url()
+  if url:match("%w://") then
+    return do_open(url)
+  end
+
+  -- consider anything that looks like string/string a github link
+  local plugin_url_regex = "[%a%d%-%.%_]*%/[%a%d%-%.%_]*"
+  local link = string.match(url, plugin_url_regex)
+  if link then
+    return do_open(string.format("https://www.github.com/%s", link))
+  end
+
+  vim.notify("No link found under cursor")
+end
+
+vim.keymap.set("n", "gx", open_link)
+vim.keymap.set("n", "gf", "<cmd>e <cfile><cr>", { desc = "Open File" })
+
+--  ╭──────────╮
+--  │ Commands │
+--  ╰──────────╯
+util.command("ToggleBackground", function()
+  vim.o.background = vim.o.background == "dark" and "light" or "dark"
+end)
+
+-- Swap clangd compile commands
+
+local function swap_compilecommands()
+  local json = require("json")
+  -- take rootdir/compile_commands.json and swap the two entries
+  local rootdir = vim.fn.getcwd()
+  local file = rootdir .. "/compile_commands.json"
+  local f = io.open(file, "r")
+  if not f then
+    vim.notify("No compile_commands.json found")
+    return
+  end
+  local data = f:read("*all")
+  f:close()
+  local commands = json.decode(data)
+  commands[1], commands[2] = commands[2], commands[1]
+
+  f = io.open(file, "w")
+  if not f then
+    vim.notify("Could not open compile_commands.json for writing!")
+    return
+  end
+  f:write(json.encode(commands))
+  f:close()
+  vim.notify("Swapped compile_commands.json!")
+end
+
+local function swap_compilecommands2()
+  local shell_code = [=[
+#!/usr/bin/env bash
+
+compile_commands_file="$PWD/compile_commands.json"
+echo "$compile_commands_file"
+tmp_file=$(mktemp)
+
+jq '[.[1], .[0]]' "$compile_commands_file" >"$tmp_file" && mv "$tmp_file" "$compile_commands_file"
+]=]
+  local tmp_file = vim.fn.tempname()
+  local f = io.open(tmp_file, "w")
+  if not f then
+    vim.notify("Could not open tmp_file for writing!")
+    return
+  end
+  f:write(shell_code)
+  f:close()
+  vim.fn.jobstart({ "sh", tmp_file }, { detach = true })
+  vim.notify("Swapped compile_commands.json!")
+end
+
+-- vim.keymap.set("n", "<leader>clf", swap_compilecommands2, { desc = "Swap Compile Commands" })
+
+local watch_type = require("vim._watch").FileChangeType
+
+local function handler(res, callback)
+  if not res.files or res.is_fresh_instance then
+    return
+  end
+
+  for _, file in ipairs(res.files) do
+    local path = res.root .. "/" .. file.name
+    local change = watch_type.Changed
+    if file.new then
+      change = watch_type.Created
+    end
+    if not file.exists then
+      change = watch_type.Deleted
+    end
+    callback(path, change)
+  end
+end
+
+function watchman(path, opts, callback)
+  vim.system({ "watchman", "watch", path }):wait()
+
+  local buf = {}
+  local sub = vim.system({
+    "watchman",
+    "-j",
+    "--server-encoding=json",
+    "-p",
+  }, {
+    stdin = vim.json.encode({
+      "subscribe",
+      path,
+      "nvim:" .. path,
+      {
+        expression = { "anyof", { "type", "f" }, { "type", "d" } },
+        fields = { "name", "exists", "new" },
+      },
+    }),
+    stdout = function(_, data)
+      if not data then
+        return
+      end
+      for line in vim.gsplit(data, "\n", { plain = true, trimempty = true }) do
+        table.insert(buf, line)
+        if line == "}" then
+          local res = vim.json.decode(table.concat(buf))
+          handler(res, callback)
+          buf = {}
+        end
+      end
+    end,
+    text = true,
+  })
+
+  return function()
+    sub:kill("sigint")
+  end
+end
+
+if vim.fn.executable("watchman") == 1 then
+  require("vim.lsp._watchfiles")._watchfunc = watchman
+else
+  vim.notify("watchman not found, using default filewatcher")
+end
---
+++ nvim/lua/plugins/tools.lua
@@ -0,0 +1,209 @@
+return {
+  { "pwntester/octo.nvim", cmd = "Octo", opts = {} },
+
+  -- -- better diffing
+  -- {
+  --   "sindrets/diffview.nvim",
+  --   cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
+  --   opts = {},
+  --   keys = { { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "DiffView" } },
+  -- },
+
+  {
+    "nacro90/numb.nvim",
+    event = "CmdlineEnter",
+    config = function()
+      require("numb").setup()
+    end,
+  },
+
+  {
+    "psliwka/vim-dirtytalk",
+    build = ":DirtytalkUpdate",
+    config = function()
+      vim.opt.spelllang:append("programming")
+    end,
+  },
+
+  "wellle/targets.vim",
+
+  { "rafcamlet/nvim-luapad", cmd = "Luapad" },
+
+  {
+    "bennypowers/nvim-regexplainer",
+    event = "BufRead",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "MunifTanjim/nui.nvim" },
+    config = true,
+  },
+
+  {
+    "~p00f/godbolt.nvim",
+    url = "https://git.sr.ht/~p00f/godbolt.nvim",
+    cmd = { "Godbolt", "GodboltCompiler" },
+  },
+
+  {
+    "t-troebst/perfanno.nvim",
+    opts = function()
+      local util = require("perfanno.util")
+      local hl = vim.api.nvim_get_hl(0, { name = "Normal" })
+      local bg = string.format("#%06x", hl.bg)
+      local fg = "#dc2626"
+      return {
+        line_highlights = util.make_bg_highlights(bg, fg, 10),
+        vt_highlight = util.make_fg_highlight(fg),
+      }
+    end,
+    keys = {
+      { "<leader>alf", "<cmd>PerfLoadFlat<cr>", desc = "Load flat profile" },
+      { "<leader>alg", "<cmd>PerfLoadCallGraph<cr>", desc = "Load call graph" },
+      { "<leader>alo", "<cmd>PerfLoadFlameGraph<cr>", desc = "Load flame graph" },
+      { "<leader>ae", "<cmd>PerfPickEvent<cr>", desc = "Pick event" },
+      { "<leader>aa", "<cmd>PerfAnnotate<cr>", desc = "Annotate" },
+      { "<leader>af", "<cmd>PerfAnnotateFunction<cr>", desc = "Annotate function" },
+      { "<leader>at", "<cmd>PerfToggleAnnotations<cr>", desc = "Toggle annotations" },
+      { "<leader>ah", "<cmd>PerfHottestLines<cr>", desc = "Hottest lines" },
+      { "<leader>as", "<cmd>PerfHottestSymbols<cr>", desc = "Hottest symbols" },
+      { "<leader>ac", "<cmd>PerfHottestCallersFunction<cr>", desc = "Hottest callers function" },
+      { "<leader>all", "<cmd>PerfLuaProfileStart<cr>", desc = "Start Lua Profiler" },
+      { "<leader>als", "<cmd>PerfLuaProfileStop<cr>", desc = "Stop Lua Profiler" },
+    },
+    cmd = {
+      "PerfLuaProfileStart",
+      "PerfLuaProfileStop",
+      "PerfLoadFlat",
+      "PerfLoadCallGraph",
+      "PerfLoadFlameGraph",
+      "PerfCacheLoad",
+      "PerfCacheSave",
+      "PerfCacheDelete",
+      "PerfCycleFormat",
+    },
+  },
+
+  {
+    "andweeb/presence.nvim",
+    event = "BufRead",
+    opts = {
+      main_image = "file",
+      enable_line_number = true,
+      blacklist = {
+        ".*zsh.*",
+        ".*ToggleTerm.*",
+        ".*toggleterm.*",
+      },
+      buttons = function(_, repo_url)
+        if repo_url then
+          if repo_url:match("amaanq") then
+            return nil
+          end
+          -- Check if repo url uses short ssh syntax
+          local domain, project = repo_url:match("^git@(.+):(.+)$")
+          if domain and project then
+            repo_url = string.format("https://%s/%s", domain, project)
+          end
+
+          -- Check if repo url uses a valid protocol
+          local protocols = {
+            "ftp",
+            "git",
+            "http",
+            "https",
+            "ssh",
+          }
+          local protocol, relative = repo_url:match("^(.+)://(.+)$")
+          if not vim.tbl_contains(protocols, protocol) or not relative then
+            return nil
+          end
+
+          -- Check if repo url has the user specified
+          local user, path = relative:match("^(.+)@(.+)$")
+          if user and path then
+            repo_url = string.format("https://%s", path)
+          else
+            repo_url = string.format("https://%s", relative)
+          end
+
+          return {
+            { label = "View Repository", url = repo_url },
+          }
+        end
+
+        return nil
+      end,
+      ---@param filename string
+      editing_text = function(filename)
+        -- if in .config/nvim, return "Editing dotfiles"
+        -- if in ~/projects/treesitter/*/grammar.js, return "Editing a tree-sitter grammar"
+        -- else return "Editing %s"
+        if filename:match(".config/nvim") then
+          return "Editing my dotfiles"
+        elseif filename:match("projects/treesitter/.+/grammar.js") then
+          -- try and get the name from the * part, if it's tree-sitter-{name}, else just return "Editing a tree-sitter grammar"
+          local name = filename:match("projects/treesitter/.+/grammar.js"):match("tree%-sitter%-(.+)")
+          if name then
+            return string.format("Editing %s's tree-sitter grammar", name)
+          end
+          return "Editing a tree-sitter grammar"
+        else
+          return string.format("Editing %s", filename)
+        end
+      end,
+      reading_text = function(buf_name) --- @param buf_name string
+        local toggleterm_process = buf_name:match("([^;]+);#toggleterm#%d+")
+
+        -- Terminal check
+        if toggleterm_process == "lazygit" then
+          return "Messing with git"
+        elseif toggleterm_process == "zsh" then
+          return "In the terminal"
+        end
+
+        local lazygit_process = buf_name:match("%d+:(.+)")
+        if lazygit_process == "lazygit" then
+          return "In Lazygit"
+        end
+
+        return string.format("Reading %s", buf_name)
+      end,
+    },
+  },
+  {
+    "nvim-neorg/neorg",
+    build = ":Neorg sync-parsers",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = { "Neorg", "NeorgOpen", "NeorgNew", "NeorgDoc" },
+    event = "LazyFile",
+    opts = {
+      load = {
+        ["core.defaults"] = {},
+        ["core.keybinds"] = {
+          config = {
+            default_keybinds = true,
+            neorg_leader = "<leader>o",
+          },
+        },
+        ["core.completion"] = {
+          config = {
+            engine = "nvim-cmp",
+          },
+        },
+        ["core.concealer"] = {
+          config = {
+            icon_preset = "diamond",
+          },
+        },
+        ["core.dirman"] = {
+          config = {
+            workspaces = {
+              notes = "~/notes",
+              school = "~/School/notes",
+            },
+            default_workspace = "notes",
+          },
+        },
+        ["core.summary"] = {},
+      },
+    },
+  },
+}
